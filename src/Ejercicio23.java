e0990e2e30984d3507435692d3e9a1d8b4125c27
kevromber <kevromber@gva.alu.edu.es>
2024-09-20 12:21:14 +0200

    Prueba

---
+++ src/Ejercicio23.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio23 {
+    public static void main(String[] args) {
+
+        int x;
+        int contador = 0;
+
+        Scanner sc = new Scanner(System.in);
+        do {
+            x = sc.nextInt();
+            if (x > 0) {
+                contador++;
+            }
+        }while (x != 0);
+
+        System.out.println("Tienes " + contador + " numeros positivos.");
+
+    }
+}
