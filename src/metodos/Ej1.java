436450faa2c294acb8821aceb4c5b404101ab352
kevromber <kevromber@gva.alu.edu.es>
2024-10-18 12:43:09 +0200

    Metodos

---
+++ src/metodos/Ej1.java
@@ -0,0 +1,22 @@
+package metodos;
+
+public class Ej1 {
+    public static boolean esDivisible(int n, int m) {
+        if (n % m == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+
+        int n = Utilidades.leerEntero("Introduce tu primer numero: ");
+        int m = Utilidades.leerEntero("Introduce tu segundo numero: ");
+
+        if (esDivisible(n, m)) {
+            System.out.println("Tu numero es divisible.");
+        } else {
+            System.out.println("Tu numero no es divisible.");
+        }
+    }
+}
---
+++ src/metodos/Ej2.java
@@ -0,0 +1,25 @@
+package metodos;
+
+public class Ej2 {
+    public static boolean esTriangulo(int a, int b, int c) {
+        if (a > b + c) {
+            return false;
+        } else if (b > a + c) {
+            return false;
+        } else if (c > a + b) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int a = Utilidades.leerEntero("Introduzca su primer lado: ");
+        int b = Utilidades.leerEntero("Introduzca su segundo lado: ");
+        int c = Utilidades.leerEntero("Introduzca su tercer lado: ");
+
+        System.out.println(esTriangulo(a, b, c));
+
+    }
+}
